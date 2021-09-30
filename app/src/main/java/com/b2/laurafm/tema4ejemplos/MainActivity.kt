805c8d70fa641107e2b38f5deb27bb7986b41243
LauraFernandez <laura.fernandezmol@iesdoctorbalmis.com>
2021-09-30 08:07:25 +0200

    Initial commit

---
+++ app/src/main/java/com/b2/laurafm/tema4ejemplos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.b2.laurafm.tema4ejemplos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout)
+    }
+}
